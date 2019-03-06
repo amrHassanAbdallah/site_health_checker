966942548329765740df2d3f2855c776724e250b
Amr Hassan <amrh434@gmail.com>
2019-03-06 12:19:31 +0200

    feat : sending requests without multiple worker.

--- main.go
+++ main.go
@@ -2,31 +2,28 @@ package main
 
 import (
      "fmt"
-     "os" //  is for accesing command line arguments
+    // "os" //  is for accesing command line arguments
      "net/http"// is for making HTTP requests
      "time"
 )
 
 
-func MakeRequest(url string, ch chan<-string) {
+func MakeRequest(url string) {
   start := time.Now()
   resp, _ := http.Get(url)
 
   secs := time.Since(start).Seconds()
 
-  ch <- fmt.Sprintf("%.2f elapsed with domain: %s with response status %d", secs, url,resp.StatusCode)
+  fmt.Sprintf("%.2f elapsed with domain: %s with response status %d", secs, url,resp.StatusCode)
 }
 
 
 func main()  {
  start := time.Now()
-  ch := make(chan string)
-  for _,url := range os.Args[1:]{
-      go MakeRequest(url, ch)
+ urls := []string{"http://localhost:8010/api/slow/test/22","http://localhost:8010/api/fast/test/1","http://localhost:8010/api/fast/test/2","http://localhost:8010/api/fast/test/3","http://localhost:8010/api/slow/test/66"}
+  for _,url := range urls{
+       MakeRequest(url)
   }
 
-  for range os.Args[1:]{
-    fmt.Println(<-ch)
-  }
   fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
 }
